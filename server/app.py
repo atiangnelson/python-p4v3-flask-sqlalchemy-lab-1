83cf97b697d42617d202b2e133d4ffd83bf11b43
Nelson <nelson.atiang@student.moringaschool.com>
2025-06-23 21:49:59 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response,jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,7 +21,33 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake =Earthquake.query.get(id)
+    if earthquake:
+        return jsonify(earthquake.to_dict())
+    else:
+        return jsonify({"message": f"Earthquake {id} not found."}),404
 
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')   
+def  get_earthquake_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude>=magnitude).all()
+    result=[]
+    for eq in earthquakes:
+        result.append({
+            'id':eq.id,
+            'location':eq.location,
+            'magnitude':eq.magnitude,
+            'year':eq.year
+        })    
+
+    response={'count':len(result),
+                  'quakes':result
+        
+        }
+
+    return jsonify(response),200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,9 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData,Integer,Float,String,Column
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
+class Earthquake(db.Model,SerializerMixin):
+    __tablename__="earthquakes"
+    id=Column(Integer,primary_key=True)
+    magnitude=Column(Float)
+    location=Column(String)
+    year=Column(Integer)
 # Add models here
+    def __repr__(self):
+        return f'{self.id},{self.magnitude},{self.location},{self.year}' 
